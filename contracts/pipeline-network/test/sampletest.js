b49f613eec2052e6669241a45a7d8223e5f1d087
ukml2018 <ukmlcal@gmail.com>
2018-11-24 23:58:51 -0500

    Added contract

--- contracts/pipeline-network/test/sampletest.js
+++ contracts/pipeline-network/test/sampletest.js
@@ -1,3 +1,4 @@
+/*eslint strict: [2, "never"]*/
 /*
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -10,9 +11,4 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
-
-'use strict';
-/**
- * Write the unit tests for your transction processor functions here
  */
