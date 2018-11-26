3aaf678d14d63951ff2ba8afc02b2c317f3201d2
ukml2018 <ukmlcal@gmail.com>
2018-11-25 23:14:40 -0500

    Added contract

--- .bluemix/pipeline-COMMON.sh
+++ .bluemix/pipeline-COMMON.sh
@@ -61,7 +61,7 @@ function install_jq {
 
 function do_curl {
     HTTP_RESPONSE=$(mktemp)
-    HTTP_STATUS=$(curl -k -w '%{http_code}' -o ${HTTP_RESPONSE} "$@")
+    HTTP_STATUS=$(curl -w '%{http_code}' -o ${HTTP_RESPONSE} "$@")
     cat ${HTTP_RESPONSE}
     rm -f ${HTTP_RESPONSE}
     if [[ ${HTTP_STATUS} -ge 200 && ${HTTP_STATUS} -lt 300 ]]
